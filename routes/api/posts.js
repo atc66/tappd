7ab151c12fc2527c91c4f0ed538e951136a20528
aciukurescu <andrewciukurescu@gmail.com>
2018-06-26 17:10:41 -0400

    POST posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-// @Route GET api/posts/test
-// @DESC test posts route
+// Post Models
+const Post = require("../../models/Post");
+
+// Post validation
+const validatePostInput = require("../../validation/post");
+
+// @route   GET api/posts/test
+// @desc    Tests post route
+// @access  Public route
+router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @Route POST api/posts
+// @DESC Create posts route
 // @access public
-router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
