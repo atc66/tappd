9e75a1f61e741714a20753a6686fc1027b84fcfe
aciukurescu <andrewciukurescu@gmail.com>
2018-06-27 10:51:04 -0400

    add and delete comments from posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -143,4 +143,66 @@ router.post(
     });
   }
 );
+
+// @Route POST api/posts/comment/:id
+// @DESC Comment posts route
+// @access private
+
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          user: req.user.id
+        };
+        // Add to comment array
+        post.comments.unshift(newComment);
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No Post found" }));
+  }
+);
+
+// @Route DELETE api/posts/comment/:id/:comment_id
+// @DESC REMOVE Comment posts route
+// @access private
+
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: "That comment does not exist." });
+        }
+        // Remove from index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+        // Splice from comment array
+        post.comments.splice(removeIndex, 1);
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No Post found" }));
+  }
+);
+
 module.exports = router;
