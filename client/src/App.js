a34d5092091f23d15d9ecb979a5ac50da84fe3c1
aciukurescu <andrewciukurescu@gmail.com>
2018-06-29 01:10:44 -0400

    profile routes component

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,9 @@ import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
 
 import "./App.css";
 import "./grid.css";
@@ -55,6 +58,8 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
@@ -72,6 +77,7 @@ class App extends Component {
                   component={EditProfile}
                 />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
+
+class ProfileAbout extends Component {
+  render() {
+    const { profile } = this.props;
+
+    const firstName = profile.user.name.trim().split(" ")[0];
+    return (
+      <div className="row">
+        <div>
+          <div className="row">
+            <h3>{firstName}'s Bio</h3>
+            <p>{isEmpty(profile.bio) ? null : <span>{profile.bio}</span>}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
+export default ProfileAbout;
